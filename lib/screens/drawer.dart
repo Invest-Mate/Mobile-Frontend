152232567ddbd0013e1b196867cfbfcaceeb32af
Pranay Dhongade <77525888+PranayD1807@users.noreply.github.com>
2022-03-30 17:07:48 +0530

    Drawer Added

--- lib/screens/drawer.dart
+++ lib/screens/drawer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'auth/signup_screen.dart';
@@ -10,40 +11,115 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 1,
+          foregroundColor: Colors.black,
+          centerTitle: true,
+          title: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text(
+                'Dash',
+                style: TextStyle(
+                  color: Color.fromRGBO(254, 161, 21, 1),
+                ),
+              ),
+              Text(
+                'Board',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              )
+            ],
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            ElevatedButton(
-              onPressed: () {
+            DrawerListTile(
+              icon: CupertinoIcons.person_alt_circle,
+              title: 'My Profile',
+              onTap: () {
                 Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const CampaignDetailScreen(),
-                  ),
+                  MaterialPageRoute(builder: (context) => ProfileScreen()),
                 );
               },
-              child: const Text('DETAIL PAGE'),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const ProfileScreen(),
-                  ),
-                );
-              },
-              child: const Text('Profile Page'),
+            const Divider(height: 3, thickness: 2, endIndent: 15, indent: 15),
+            DrawerListTile(
+              title: 'My Fundraisers',
+              onTap: () {},
+              isImageIcon: true,
+              imagePath: 'assets/images/strongbox.png',
             ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                        builder: (context) => const SignUpScreen()),
-                  );
-                },
-                child: const Text('Sign Up')),
+            const Divider(height: 3, thickness: 2, endIndent: 15, indent: 15),
+            DrawerListTile(
+              icon: Icons.monetization_on_outlined,
+              title: 'Transactions',
+              onTap: () {},
+            ),
+            const Divider(height: 3, thickness: 2, endIndent: 15, indent: 15),
+            DrawerListTile(
+              isImageIcon: true,
+              imagePath: 'assets/images/book.png',
+              title: 'My Carts',
+              onTap: () {},
+            ),
+            const Divider(height: 3, thickness: 2, endIndent: 15, indent: 15),
+            DrawerListTile(
+              icon: Icons.logout,
+              title: 'Log Out',
+              onTap: () {},
+            ),
+            Expanded(
+                child: Center(
+              child: Image.asset(
+                'assets/images/img1.png',
+                height: 250,
+              ),
+            ))
           ],
         ),
+      ),
+    );
+  }
+}
+
+class DrawerListTile extends StatelessWidget {
+  const DrawerListTile({
+    Key? key,
+    this.icon,
+    this.isImageIcon = false,
+    this.imagePath = '',
+    required this.title,
+    this.onTap,
+  }) : super(key: key);
+  final IconData? icon;
+  final bool isImageIcon;
+  final String title;
+  final String imagePath;
+  final Function()? onTap;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onTap,
+      leading: isImageIcon
+          ? ImageIcon(
+              AssetImage(imagePath),
+              size: 35,
+              color: const Color.fromRGBO(254, 161, 21, 1),
+            )
+          : Icon(
+              icon,
+              size: 35,
+              color: const Color.fromRGBO(254, 161, 21, 1),
+            ),
+      title: Text(
+        title,
+        textScaleFactor: 1.5,
+        style: const TextStyle(fontWeight: FontWeight.w400),
       ),
     );
   }
--- lib/screens/home/latest_fund_item.dart
+++ lib/screens/home/latest_fund_item.dart
@@ -1,3 +1,4 @@
+import 'package:crowd_application/screens/campaign_detail/campaign_detail_screen.dart';
 import 'package:flutter/material.dart';
 
 class LatestFundItem extends StatelessWidget {
@@ -20,112 +21,123 @@ class LatestFundItem extends StatelessWidget {
     final timeLeft = lastDate.difference(DateTime.now()).inDays;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
-      child: Container(
-        height: 100,
-        clipBehavior: Clip.antiAlias,
-        decoration: BoxDecoration(
-            color: Colors.transparent,
-            borderRadius: BorderRadius.circular(15),
-            border: Border.all(
-                width: 1, color: const Color.fromRGBO(197, 197, 197, 1))),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(
-              height: 100,
-              width: 120,
-              child: Image.network(
-                imageUrl,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      title,
-                      maxLines: 1,
-                      textScaleFactor: 1.2,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 15),
-                      child: SliderTheme(
-                        data: SliderThemeData(
-                          overlayShape: SliderComponentShape.noOverlay,
-                          disabledActiveTrackColor:
-                              const Color.fromRGBO(254, 161, 21, 0.81),
-                          thumbShape: SliderComponentShape.noThumb,
-                        ),
-                        child: Slider(
-                          value: progress,
-                          onChanged: null,
-                        ),
-                      ),
-                    ),
-                    Row(
-                      // mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Icon(
-                              Icons.monetization_on_outlined,
-                              size: 15,
-                              color: Color.fromRGBO(147, 147, 147, 1),
-                            ),
-                            const SizedBox(
-                              width: 2.5,
-                            ),
-                            Text(
-                              (progress * 100).toStringAsFixed(0) + '%',
-                              textScaleFactor: 0.9,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.w600,
-                              ),
-                            )
-                          ],
-                        ),
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Icon(
-                              Icons.access_time,
-                              size: 15,
-                              color: Color.fromRGBO(147, 147, 147, 1),
-                            ),
-                            const SizedBox(
-                              width: 2.5,
-                            ),
-                            Text(
-                              timeLeft.isNegative
-                                  ? 'Expired'
-                                  : timeLeft > 28
-                                      ? '${(timeLeft / 30).round()} Months left'
-                                      : '$timeLeft Days left',
-                              textScaleFactor: 0.9,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.w600,
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    )
-                  ],
+      child: InkWell(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        hoverColor: Colors.transparent,
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+                builder: (context) => const CampaignDetailScreen()),
+          );
+        },
+        child: Container(
+          height: 100,
+          clipBehavior: Clip.antiAlias,
+          decoration: BoxDecoration(
+              color: Colors.transparent,
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(
+                  width: 1, color: const Color.fromRGBO(197, 197, 197, 1))),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                height: 100,
+                width: 120,
+                child: Image.network(
+                  imageUrl,
+                  fit: BoxFit.cover,
                 ),
               ),
-            )
-          ],
+              Expanded(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        title,
+                        maxLines: 1,
+                        textScaleFactor: 1.2,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        child: SliderTheme(
+                          data: SliderThemeData(
+                            overlayShape: SliderComponentShape.noOverlay,
+                            disabledActiveTrackColor:
+                                const Color.fromRGBO(254, 161, 21, 0.81),
+                            thumbShape: SliderComponentShape.noThumb,
+                          ),
+                          child: Slider(
+                            value: progress,
+                            onChanged: null,
+                          ),
+                        ),
+                      ),
+                      Row(
+                        // mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Icon(
+                                Icons.monetization_on_outlined,
+                                size: 15,
+                                color: Color.fromRGBO(147, 147, 147, 1),
+                              ),
+                              const SizedBox(
+                                width: 2.5,
+                              ),
+                              Text(
+                                (progress * 100).toStringAsFixed(0) + '%',
+                                textScaleFactor: 0.9,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              )
+                            ],
+                          ),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Icon(
+                                Icons.access_time,
+                                size: 15,
+                                color: Color.fromRGBO(147, 147, 147, 1),
+                              ),
+                              const SizedBox(
+                                width: 2.5,
+                              ),
+                              Text(
+                                timeLeft.isNegative
+                                    ? 'Expired'
+                                    : timeLeft > 28
+                                        ? '${(timeLeft / 30).round()} Months left'
+                                        : '$timeLeft Days left',
+                                textScaleFactor: 0.9,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
